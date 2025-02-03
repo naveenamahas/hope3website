31f94865b47bdb62773cef2ab3a2ad48419c2c51
naveenamahas <naveenamahas1712@gmail.com>
2025-02-03 16:31:30 +0530

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -14,7 +14,8 @@
   display: flex;
   justify-content:space-evenly;
   
-  background-image: url('/static/homepage.jpg');
+  /* background-image: url('/static/homepage.jpg'); */
+  background-color: #ffb700;
 
   padding: 90px;
   border-radius: 2px; 
